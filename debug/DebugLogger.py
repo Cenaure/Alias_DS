f3126dbd12720a4edfe08a020f0f751db0b0abe2
Cenaure <127963429+Cenaure@users.noreply.github.com>
2026-04-08 23:57:53 +0200

    Delete debug directory

--- debug/DebugLogger.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-class DebugLogger:
-    @staticmethod
-    def Console(message, *args):
-        time = datetime.now()
-        print(f"| {time.hour}:{time.minute}:{time.second} | {message} {args}")
